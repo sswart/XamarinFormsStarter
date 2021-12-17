582bbdce1255cce8565e3d9c17032cc8c1b23a15
Jesse Swart <jswart@xpirit.com>
2021-12-17 10:25:56 +0100

    Add project files.

---
+++ src/BlogApp/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.ViewModels;
+using BlogApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BlogApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/BlogApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlogApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+    public static class AboutPageAutomation
+    {
+        public static string Title = "title";
+    }
+}
